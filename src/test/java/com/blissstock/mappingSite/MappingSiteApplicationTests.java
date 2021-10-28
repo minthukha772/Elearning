096382510a2f9249951ac5e8b85fd8217505ab24
Sai Horm Kham <saihormkham1998@gmail.com>
2021-10-28 19:00:35 +0630

    init project

---
+++ src/test/java/com/blissstock/mappingSite/MappingSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blissstock.mappingSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
