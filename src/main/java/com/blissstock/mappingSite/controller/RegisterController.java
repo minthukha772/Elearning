c29d61bfe83f8d411b1167e553c0ba241759b575
Sai Horm Kham <sai.hormkham.t19@bliss-stock.com>
2021-11-02 19:25:23 +0630

    format document

--- src/main/java/com/blissstock/mappingSite/controller/RegisterController.java
+++ src/main/java/com/blissstock/mappingSite/controller/RegisterController.java
@@ -7,18 +7,12 @@ import javax.validation.Valid;
 
 import com.blissstock.mappingSite.dto.TeacherRegisterDTO;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @Controller
--- src/main/java/com/blissstock/mappingSite/dto/TeacherRegisterDTO.java
+++ src/main/java/com/blissstock/mappingSite/dto/TeacherRegisterDTO.java
@@ -5,71 +5,81 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public @Data class TeacherRegisterDTO {
+@Data
+public class TeacherRegisterDTO {
 
-    @Email
-    private String email;
+  @Email
+  private String email;
 
-    @NotBlank(message = "This field is required")
-    private String name;
+  @NotBlank(message = "This field is required")
+  private String name;
 
-    @NotBlank(message = "This field is required")
-    @Size(min = 8, message = "Password must be at least 8 characters")
-    private String password;
+  @Size(min = 8, message = "Password must be at least 8 characters")
+  @Pattern(
+    //check if the string contain at least one character and one digit
+    regexp = "(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$",
+    flags = { Pattern.Flag.CASE_INSENSITIVE },
+    message = "Password must contain at least one character and one digit"
+  )
+  private String password;
 
-    @NotBlank(message = "This field is required")
-    @Size(min = 8, message = "Confirm Password must be at least 8 characters")
-    private String confirmPassword;
+  @Size(min = 8, message = "Confirm Password must be at least 8 characters")
+  @Pattern(
+    //check if the string contain at least one character and one digit
+    regexp = "(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$",
+    flags = { Pattern.Flag.CASE_INSENSITIVE },
+    message = "Password must contain at least one character and one digit"
+  )
+  private String confirmPassword;
 
-    @NotBlank(message = "This field is required")
-    private String gender;
+  @NotBlank(message = "This field is required")
+  private String gender;
 
-    @NotBlank(message = "This field is required")
-    private String phone;
+  @NotBlank(message = "This field is required")
+  private String phone;
 
-    @NotBlank(message = "This field is required")
-    private String dob;
+  @NotBlank(message = "This field is required")
+  private String dob;
 
-    @Min(value =  10000, message = "invalid format")
-    @Max(value =  99999, message = "invalid format")
-    private int zipCode;
+  @Min(value = 10000, message = "invalid format")
+  @Max(value = 99999, message = "invalid format")
+  private int zipCode;
 
-    @NotBlank(message = "This field is required")
-    private String city;
+  @NotBlank(message = "This field is required")
+  private String city;
 
-    @NotBlank(message = "This field is required")
-    private String division;
+  @NotBlank(message = "This field is required")
+  private String division;
 
-    @NotBlank(message = "This field is required")
-    private String address;
+  @NotBlank(message = "This field is required")
+  private String address;
 
-    @NotBlank(message = "This field is required")
-    private String education;
+  @NotBlank(message = "This field is required")
+  private String education;
 
-    @NotBlank(message = "This field is required")
-    private String nrc;
+  @NotBlank(message = "This field is required")
+  private String nrc;
 
-    @NotBlank(message = "This field is required")
-    private String award;
+  @NotBlank(message = "This field is required")
+  private String award;
 
-    @NotBlank(message = "This field is required")
-    private String selfDescription;
+  @NotBlank(message = "This field is required")
+  private String selfDescription;
 
-    private String kPay;
-    private String cbPay;
-    private String wave;
-    private String kbz;
-    private String cb;
-    private String aya;
+  private String kPay;
+  private String cbPay;
+  private String wave;
+  private String kbz;
+  private String cb;
+  private String aya;
 
-    private boolean isAcceptTerm;
-
+  private boolean isAcceptTerm;
 }
