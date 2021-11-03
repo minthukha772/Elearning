606143293e9c9895910b0a6bc461a52a8baeb58d
Sai Horm Kham <sai.hormkham.t19@bliss-stock.com>
2021-11-03 13:59:02 +0630

    validation

--- src/main/java/com/blissstock/mappingSite/controller/RegisterController.java
+++ src/main/java/com/blissstock/mappingSite/controller/RegisterController.java
@@ -1,41 +1,62 @@
 package com.blissstock.mappingSite.controller;
 
-
-import java.util.List;
-
-import javax.validation.Valid;
-
 import com.blissstock.mappingSite.dto.TeacherRegisterDTO;
-
+import java.util.List;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import org.hibernate.validator.HibernateValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Validated
 public class RegisterController {
-    
-    @GetMapping("/register")
-    public String form(Model model){
-        TeacherRegisterDTO userInfo = new TeacherRegisterDTO();
-        userInfo.setEmail("sai@gmail.com");
-        model.addAttribute("action", "register");
-        model.addAttribute("userInfo", userInfo);
-        return "register.html";
+
+  @ExceptionHandler(value = ConstraintViolationException.class)
+  public String exception(ConstraintViolationException exception) {
+    System.out.println("excption occur");
+    System.out.println(exception.getMessage());
+    return "redirect:/";
+  }
+
+  @GetMapping("/register/{role}/{email}")
+  public String registerForm(
+    @PathVariable(required = false) String role,
+    @Email @PathVariable String email,
+    Model model
+  ) {
+    if (role == null || role != "teacher") {
+      role = "student";
     }
 
-    @PostMapping("/check")
-    public String register(Model model,@Valid @ModelAttribute("userInfo")  TeacherRegisterDTO userInfo,  BindingResult bindingResult){
-        //model.addAttribute("userInfo", userInfo);
-        //System.out.println(bindingResult.getFieldError("passwordMatch"));
-        //System.out.println(bindingResult.toString());
-        return "register.html";
-    }
+    System.out.println(role);
+    System.out.println(email);
+    TeacherRegisterDTO userInfo = new TeacherRegisterDTO();
+    userInfo.setEmail(email);
+    model.addAttribute("action", "register");
+    model.addAttribute("role", role);
+    model.addAttribute("userInfo", userInfo);
+    return "register.html";
+  }
 
-
-
-
+  @PostMapping("/register")
+  public String register(
+    Model model,
+    @Valid @ModelAttribute("userInfo") TeacherRegisterDTO userInfo,
+    BindingResult bindingResult
+  ) {
+    //model.addAttribute("userInfo", userInfo);
+    //System.out.println(bindingResult.getFieldError("passwordMatch"));
+    //System.out.println(bindingResult.toString());
+    return "register.html";
+  }
 }
--- src/main/java/com/blissstock/mappingSite/dto/TeacherRegisterDTO.java
+++ src/main/java/com/blissstock/mappingSite/dto/TeacherRegisterDTO.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 @Getter
 @Setter
+
 @Data
 public class TeacherRegisterDTO extends UserRegisterDTO{
   
