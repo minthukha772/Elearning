234a37e8a7480019fe02bb09866c62776ddd31ea
Sai Horm Kham <sai.hormkham.t19@bliss-stock.com>
2021-11-11 17:03:40 +0630

    Add Services

---
+++ src/main/java/com/blissstock/mappingSite/repository/UserAccountRepository.java
@@ -0,0 +1,12 @@
+package com.blissstock.mappingSite.repository;
+
+import com.blissstock.mappingSite.entity.UserAccount;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserAccountRepository extends CrudRepository<UserAccount, Long> {
+    
+}
+
