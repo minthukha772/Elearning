ccdf9be18474bc85820cc612d28039f77b5fcf1a
Sai Horm Kham <sai.hormkham.t19@bliss-stock.com>
2021-11-21 21:18:18 +0630

    upload certificates

--- src/main/java/com/blissstock/mappingSite/controller/ManageCertificateController.java
+++ src/main/java/com/blissstock/mappingSite/controller/ManageCertificateController.java
@@ -1,15 +1,85 @@
 package com.blissstock.mappingSite.controller;
 
+import com.blissstock.mappingSite.exceptions.NotImageFileException;
+import com.blissstock.mappingSite.model.FileInfo;
+import com.blissstock.mappingSite.service.StorageService;
+import com.blissstock.mappingSite.validation.validators.ImageFileValidator;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 @Controller
 public class ManageCertificateController {
-    @GetMapping("/manage_certificate")
-    public String manageCertificate(Model model){
-        
-        return "AT0007_manage_certificate";
-        
+
+  @Autowired
+  StorageService storageService;
+
+  @GetMapping("/manage_certificate")
+  public String manageCertificate(Model model) {
+    List<FileInfo> fileInfos = loadImages();
+    model.addAttribute("files", fileInfos);
+
+    return "AT0007_manage_certificate";
+  }
+
+  @PostMapping(value = "/manage_certificate")
+  public String uploadFiles(
+    @RequestParam("files") MultipartFile[] files,
+    Model model
+  ) {
+    System.out.println(files.length);
+    try {
+      if (files.length > 0) {
+        storageService.storeCertificates(files);
+      } else {
+        model.addAttribute(
+          "fileUploadError",
+          "Please Select at least one file"
+        );
+      }
+    } catch (NotImageFileException e) {
+      model.addAttribute(
+        "fileUploadError",
+        "Only Jpg, Jpeg and Png are allowed"
+      );
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+
+    List<FileInfo> fileInfos = loadImages();
+    model.addAttribute("files", fileInfos);
+
+    return "AT0007_manage_certificate";
+  }
+
+
+  //TODO Implement DELETE REQUEST
+
+  private List<FileInfo> loadImages() {
+    return storageService
+      .loadAllCertificates()
+      .map(
+        path -> {
+          String name = path.getFileName().toString();
+          String url = MvcUriComponentsBuilder
+            .fromMethodName(
+              FileController.class,
+              "getCertificates",
+              path.getFileName().toString()
+            )
+            .build()
+            .toString();
+
+          return new FileInfo(name, url);
+        }
+      )
+      .collect(Collectors.toList());
+  }
 }
