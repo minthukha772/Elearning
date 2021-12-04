2015ca50db86471a010f24cc454f05dc8c950d3f
phy0sithukyaw <69191121+phy0sithukyaw@users.noreply.github.com>
2021-12-04 11:12:13 +0630

    Added repositories required for CM0002_CourseListScreen.

---
+++ src/main/java/com/blissstock/mappingSite/repository/UserAccountRepository.java
@@ -0,0 +1,12 @@
+package com.blissstock.mappingSite.repository;
+
+import java.util.List;
+
+import com.blissstock.mappingSite.entity.UserAccount;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    
+    List<UserAccount> findByRole(String role);
+}
