0331fa31a338189cf87fcaf384bdeb5caf8f2f12
phy0sithukyaw <69191121+phy0sithukyaw@users.noreply.github.com>
2021-12-12 10:25:27 +0630

    Added combined files 2

---
+++ model/FileInfo.java
@@ -0,0 +1,15 @@
+package com.blissstock.mappingSite.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileInfo {
+    private String name;
+    private String url;
+}
