60c2031a7d66edce780b26260ff7bdbd6d5ef08a
NaylarAung99 <naylar.aung.r17@bliss-stock.com>
2021-12-14 14:03:28 +0630

    Update review

--- src/main/java/com/blissstock/mappingSite/service/TeacherReviewServiceImpl.java
+++ src/main/java/com/blissstock/mappingSite/service/TeacherReviewServiceImpl.java
@@ -1,5 +1,7 @@
 package com.blissstock.mappingSite.service;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 import javax.validation.Validation;
 import javax.validation.Validator;
@@ -8,10 +10,10 @@ import javax.validation.ValidatorFactory;
 import com.blissstock.mappingSite.dto.StudentReviewDTO;
 import com.blissstock.mappingSite.dto.TeacherReviewDTO;
 import com.blissstock.mappingSite.entity.CourseInfo;
+import com.blissstock.mappingSite.entity.JoinCourseUser;
 import com.blissstock.mappingSite.entity.Review;
-import com.blissstock.mappingSite.entity.UserInfo;
+import com.blissstock.mappingSite.repository.JoinCourseUserRepository;
 import com.blissstock.mappingSite.repository.ReviewRepository;
-import com.blissstock.mappingSite.service.TeacherReviewService;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,16 +23,22 @@ import org.springframework.stereotype.Service;
 @Transactional
 public class TeacherReviewServiceImpl implements TeacherReviewService {
 
-
   @Autowired
   private ReviewRepository reviewRepo;
 
-  public void addReview(TeacherReviewDTO trReviewDTO, CourseInfo course, UserInfo userInfo) {
+  @Autowired
+  private JoinCourseUserRepository joinRepo;
+
+  public void addReview(TeacherReviewDTO trReviewDTO, Long courseId, Long userId) {
     
     Review review = Review.fromTrReviewDTO(trReviewDTO);
-    //System.out.println(review.getFeedback());
-    review.setCourseInfo(course);
-    review.setUserInfo(userInfo);
+    List<JoinCourseUser> joins=joinRepo.findByCourseUser(courseId, userId);
+    for(JoinCourseUser join:joins){
+      review.setJoin(join);
+      reviewRepo.save(review);
+      joinRepo.save(join);
+      
+    }
 
     ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
     Validator validator = factory.getValidator();
