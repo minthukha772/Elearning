e672942ef16d3199e8b81bbc6be586e977485b5e
ZawMinbliss <96045323+ZawMinbliss@users.noreply.github.com>
2021-12-15 14:36:47 +0630

    add loggin to check Email controller

--- src/main/java/com/blissstock/mappingSite/service/UserServiceImpl.java
+++ src/main/java/com/blissstock/mappingSite/service/UserServiceImpl.java
@@ -13,8 +13,10 @@ import com.blissstock.mappingSite.repository.UserAccountRepository;
 import com.blissstock.mappingSite.repository.UserInfoRepository;
 import java.util.GregorianCalendar;
 import java.util.UUID;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,7 +24,6 @@ import org.springframework.stereotype.Service;
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
-
   @Autowired
   private final UserAccountRepository userAccountRepository;
 
@@ -38,6 +39,7 @@ public class UserServiceImpl implements UserService {
   @Autowired
   private MailService mailService;
 
+  private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
   public UserServiceImpl(
     UserAccountRepository userAccountRepository,
     UserInfoRepository userInfoRepository,
@@ -60,6 +62,7 @@ public class UserServiceImpl implements UserService {
     userAccount.setPassword(passwordEncoder.encode(userAccount.getPassword()));
 
     if (this.isUserAccountPresent(userAccount.getMail())) {
+      logger.warn("User with {} email already exists",userAccount.getMail());
       throw new UserAlreadyExistException();
     }
 
