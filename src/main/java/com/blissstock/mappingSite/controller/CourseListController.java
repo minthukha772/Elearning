197ddb51f445b37152f426a8174900e745fe8bdc
= <=>
2021-12-31 08:02:04 +0630

    CourseList update

--- src/main/java/com/blissstock/mappingSite/controller/CourseListController.java
+++ src/main/java/com/blissstock/mappingSite/controller/CourseListController.java
@@ -1,4 +1,4 @@
-/* package com.blissstock.mappingSite.controller;
+package com.blissstock.mappingSite.controller;
 
 import org.checkerframework.checker.guieffect.qual.UI;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -22,9 +23,11 @@ import java.util.Optional;
 
 import com.blissstock.mappingSite.entity.CourseInfo;
 import com.blissstock.mappingSite.entity.CourseTime;
+import com.blissstock.mappingSite.entity.JoinCourseUser;
 import com.blissstock.mappingSite.entity.UserAccount;
 import com.blissstock.mappingSite.entity.UserInfo;
 import com.blissstock.mappingSite.repository.CourseRepository;
+import com.blissstock.mappingSite.repository.JoinCourseUserRepository;
 import com.blissstock.mappingSite.repository.UserAccountRepository;
 import com.blissstock.mappingSite.repository.UserInfoRepositoryTwo;
 
@@ -43,6 +46,9 @@ public class CourseListController {
 
     @Autowired
     private UserInfoRepositoryTwo userInfoRepo;
+
+    @Autowired
+    private JoinCourseUserRepository joinCourseUserRepo;
     
     @RequestMapping("/guest-course-list")
     private String getCourseListGuest(Model model){
@@ -77,9 +83,21 @@ public class CourseListController {
     }
 
     @RequestMapping("/teacher-course-list")
-    private String getCourseListTeacher(@RequestParam("tId") Long tId, Model model){
-        UserInfo userInfo = userInfoRepo.findById(tId).get();
-        List<CourseInfo> courseList = userInfo.getCourseInfo();
+    private String getCourseListTeacher(Model model, Principal principal){
+        UserAccount userAccount = userAccountRepo.findByMail(principal.getName());
+
+        System.out.println("Thiha sout yuu" + principal.getName());
+        UserInfo userInfo = userInfoRepo.findByUserAccount(userAccount);
+        List<JoinCourseUser> joinCourseList = joinCourseUserRepo.findByUserInfo(userInfo);
+        List<CourseInfo> courseList = new ArrayList<>();
+        for(JoinCourseUser joinCourseUser : joinCourseList){
+            courseList.add(joinCourseUser.getCourseInfo());
+        }
+        if(courseList.size() == 0){
+            model.addAttribute("something", false);
+        }else {
+            model.addAttribute("something", true);
+        }
         model.addAttribute("userInfo", userInfo);
         model.addAttribute("courseList", courseList);
         return "CM0002_CourseListTeacher";
@@ -131,12 +149,12 @@ public class CourseListController {
 
           
             for(UserInfo userInfo : userInfoList){
-                List<CourseInfo> cInfo = courseRepo.findByUserInfo(userInfo);
-               
-                for(CourseInfo courseInfo1 : cInfo){
-                    courseList.add(courseInfo1);
-          
+                List<JoinCourseUser> joinCourseUserList = joinCourseUserRepo.findByUserInfo(userInfo);
+
+                for(JoinCourseUser joinCourseUser : joinCourseUserList){
+                    courseList.add(joinCourseUser.getCourseInfo());
                 }
+               
             }
 
             if(courseList.size()==0){
@@ -166,14 +184,15 @@ public class CourseListController {
 
           
             for(UserInfo userInfo : userInfoList){
-                List<CourseInfo> cInfo = courseRepo.findByUserInfo(userInfo);
+                // List<CourseInfo> cInfo = courseRepo.findByUserInfo(userInfo);
+                List<JoinCourseUser> joinCourseUserList = joinCourseUserRepo.findByUserInfo(userInfo);
                
-                for(CourseInfo courseInfo1 : cInfo){
-                    if(courseInfo1.getCourseName().equals(courseName)){
-                        courseList.add(courseInfo1);
+                for(JoinCourseUser joinCourseUser : joinCourseUserList){
+                    if(joinCourseUser.getCourseInfo().getCourseName().equals(courseName)){
+                        courseList.add(joinCourseUser.getCourseInfo());
                     }
-
                 }
+
             }
             
             if(courseList.size()==0){
@@ -203,8 +222,12 @@ public class CourseListController {
             
             for(UserInfo userInfo : userInfoList){
                 
-                List<CourseInfo> cInfo = courseRepo.findByUserInfo(userInfo);
+                List<CourseInfo> cInfo = new ArrayList<>();
+                List<JoinCourseUser> joinCourseUserList = joinCourseUserRepo.findByUserInfo(userInfo);
                
+                for(JoinCourseUser joinCourseUser : joinCourseUserList){
+                    cInfo.add(joinCourseUser.getCourseInfo());
+                }
                 
                 for(CourseInfo courseInfo1 : cInfo){
                     
@@ -242,7 +265,12 @@ public class CourseListController {
             
             for(UserInfo userInfo : userInfoList){
                 
-                List<CourseInfo> cInfo = courseRepo.findByUserInfo(userInfo);
+                List<CourseInfo> cInfo = new ArrayList<>();
+                List<JoinCourseUser> joinCourseUserList = joinCourseUserRepo.findByUserInfo(userInfo);
+               
+                for(JoinCourseUser joinCourseUser : joinCourseUserList){
+                    cInfo.add(joinCourseUser.getCourseInfo());
+                }
                
                 
                 for(CourseInfo courseInfo1 : cInfo){
@@ -266,16 +294,7 @@ public class CourseListController {
             
         }
 
-        // else if(courseName=="" && teacherName=="" && fromDate!="" && toDate==""){
-        //     // DateFormat nextFormatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-        //     // String startDate = fromDate + " 00:00:00";
-        //     Date startDate= new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(fromDate);  
-        //     // Date startDate1 = startDate;
-
-        //     List<CourseInfo> courseList = courseRepo.findByStartDate(startDate);
-        //     model.addAttribute("courseList", courseList);
-        // }
-
+       
         
        
         return "CM0002_CourseListGuest";
@@ -351,8 +370,7 @@ public class CourseListController {
 
 
 
-        // System.out.println(levelList.get(0));
-        // System.out.println(levelList.get(1));
+  
         return "CM0002_CourseListGuest";
     }
 
@@ -363,7 +381,7 @@ public class CourseListController {
 
     @RequestMapping("/courseInfo")
     private String courseRegister(Model model){
-        // CourseInfo courseInfo = courseRepo.findById((long) 1).get();
+       
         CourseInfo courseInfo = new CourseInfo();
         model.addAttribute("course", courseInfo);
         return "test";
@@ -428,145 +446,40 @@ public class CourseListController {
         return "helloworld";
     }
 
-    @PostMapping("/userAndCourse")
-    private String userAndCourse(@ModelAttribute("uid") Long uid, @ModelAttribute("courseId") Long courseId){
+    // @PostMapping("/userAndCourse")
+    // private String userAndCourse(@ModelAttribute("uid") Long uid, @ModelAttribute("courseId") Long courseId){
         
-        CourseInfo courseInfo = courseRepo.findById(courseId).get();
-        UserInfo userInfo = userInfoRepo.findById(uid).get();
-        List<CourseInfo> courseInfoList = userInfo.getCourseInfo();
-        List<UserInfo> userInfoList = courseInfo.getUserInfo();
-        courseInfoList.add(courseInfo);
-        userInfoList.add(userInfo);
-        userInfo.setCourseInfo(courseInfoList);
-        userInfoRepo.save(userInfo);
-        courseInfo.setUserInfo(userInfoList);
-        courseRepo.save(courseInfo);
-        return "helloworld";
-    }
+    //     CourseInfo courseInfo = courseRepo.findById(courseId).get();
+    //     UserInfo userInfo = userInfoRepo.findById(uid).get();
+    //     List<CourseInfo> courseInfoList = userInfo.getCourseInfo();
+    //     List<UserInfo> userInfoList = courseInfo.getUserInfo();
+    //     courseInfoList.add(courseInfo);
+    //     userInfoList.add(userInfo);
+    //     userInfo.setCourseInfo(courseInfoList);
+    //     userInfoRepo.save(userInfo);
+    //     courseInfo.setUserInfo(userInfoList);
+    //     courseRepo.save(courseInfo);
+    //     return "helloworld";
+    // }
 
-    @RequestMapping("/hello")
-    private String helloTest(){
-        Date date = new Date(); 
-		// UserInfo userInfo = new UserInfo();
-		// userInfo.setAddress("Yangon");
-		// userInfo.setBirthDate(date);
-		// userInfo.setCertificate("certificate");
-		// userInfo.setCity("Ygn");
-		// userInfo.setDivision("Thingangyun");
-		// userInfo.setEducation("BED");
-		// userInfo.setGender("Female");
-		// userInfo.setNrc("nrc");
-		// userInfo.setPhoneNo("09788999999");
-		// userInfo.setPostalCode("11111");
-		// userInfo.setSelfDescription("This is me");
-		// userInfo.setUserName("Daw Aye");
+    // @RequestMapping("/hello")
+    // private String helloTest(){
+    //     Date date = new Date(); 
+	
+        
+    //     UserInfo helloUser = userInfoRepo.findById((long) 13).get();
+    //     for(int i=0; i<helloUser.getCourseInfo().size(); i++){
+    //         System.out.println("YayYay" + helloUser.getCourseInfo().get(i).getCourseName());
+    //     }
 
-		// UserAccount userAccount = new UserAccount();
-		// userAccount.setAccountStatus("status");
-		// userAccount.setMailVerified(true);
-		// userAccount.setMail("abc@gmail.com");
-		// userAccount.setPassword("1234");
-		// userAccount.setPhoto("/abc");
-		// userAccount.setRegisteredDate(date);
-		// userAccount.setRole("teacher");
-
-		// // userAccountRepo.save(userAccount);
-		// userInfo.setUserAccount(userAccount);
-
-		// UserInfo userInfo1 = new UserInfo();
-		// userInfo1.setAddress("Yangon");
-		// userInfo1.setBirthDate(date);
-		// userInfo1.setCertificate("certificate");
-		// userInfo1.setCity("Ygn");
-		// userInfo1.setDivision("Thingangyun");
-		// userInfo1.setEducation("BED");
-		// userInfo1.setGender("Female");
-		// userInfo1.setNrc("nrc");
-		// userInfo1.setPhoneNo("09788999999");
-		// userInfo1.setPostalCode("11111");
-		// userInfo1.setSelfDescription("This is me");
-		// userInfo1.setUserName("Daw Aye Thaung");
-
-		// UserAccount userAccount1 = new UserAccount();
-		// userAccount1.setAccountStatus("status");
-		// userAccount1.setMailVerified(true);
-		// userAccount1.setMail("abc@gmail.com");
-		// userAccount1.setPassword("1234");
-		// userAccount1.setPhoto("/abc");
-		// userAccount1.setRegisteredDate(date);
-		// userAccount1.setRole("teacher");
-
-		// // userAccountRepo.save(userAccount1);
-		// userInfo1.setUserAccount(userAccount1);
-
-		// CourseInfo cInfo = new CourseInfo();
-		// cInfo.setAboutCourse("aboutCourse");
-		// cInfo.setCategory("Programming");
-		// cInfo.setClassLink("localhost");
-		// cInfo.setClassType("Live");
-		// cInfo.setCourseName("Basic Programming");
-		// cInfo.setEndDate(date);
-		// cInfo.setFees(100);
-		// cInfo.setIsCourseApproved("true");
-		// cInfo.setLevel("Beginner");
-		// cInfo.setStartDate(date);
-
-		// CourseInfo cInfo1 = new CourseInfo();
-		// cInfo1.setAboutCourse("aboutCourse");
-		// cInfo1.setCategory("Programming");
-		// cInfo1.setClassLink("localhost");
-		// cInfo1.setClassType("Live");
-		// cInfo1.setCourseName("Intermediate Programming");
-		// cInfo1.setEndDate(date);
-		// cInfo1.setFees(100);
-		// cInfo1.setIsCourseApproved("true");
-		// cInfo1.setLevel("Beginner");
-		// cInfo1.setStartDate(date);
-
-		// userInfo.set(cInfo);
-		// userInfo.set(cInfo1);
-		
-		// cInfo.set(userInfo);
-		// cInfo1.getUserInfo().add(userInfo);
-
-		// userInfoRepo.save(userInfo);
-		// userInfoRepo.save(userInfo1);
-
-		// courseRepo.save(cInfo);
-		// courseRepo.save(cInfo1);
-
-        // List<CourseInfo> cInfoList =  courseRepo.findAll();
-        // cInfoList.add(cInfo);
-        // cInfoList.add(cInfo1);
-
-        // Long uInfoId = (long) 7;
-        // UserInfo uInfo = userInfoRepo.findById(uInfoId).get();
-        // uInfo.setCourseInfo(cInfoList);
-        // userInfoRepo.save(uInfo);
-        // System.out.println("Heehee" + userInfo);
-        // System.out.print("BlaBla" + cInfo.getUserInfo());
-
-
-        // List<UserInfo> userInfo = userInfoRepo.findAll();
-        // CourseInfo cInfo = courseRepo.findById((long) 10).get();
-        // cInfo.setUserInfo(userInfo);
-        // courseRepo.save(cInfo);
-        // System.out.println("SthSth" + userInfo.get(0).getCourseInfo());
+    //     CourseInfo cInfo = courseRepo.findById((long) 10).get();
+    //     for(int i=0; i<cInfo.getUserInfo().size(); i++){
+    //         System.out.println("YayYay" + cInfo.getUserInfo().get(i).getUserName());
+    //     }
 
         
-        UserInfo helloUser = userInfoRepo.findById((long) 13).get();
-        for(int i=0; i<helloUser.getCourseInfo().size(); i++){
-            System.out.println("YayYay" + helloUser.getCourseInfo().get(i).getCourseName());
-        }
-
-        CourseInfo cInfo = courseRepo.findById((long) 10).get();
-        for(int i=0; i<cInfo.getUserInfo().size(); i++){
-            System.out.println("YayYay" + cInfo.getUserInfo().get(i).getUserName());
-        }
-
-        
-        return "helloworld";
-    }
+    //     return "helloworld";
+    // }
 
     @GetMapping("/guest-course-details")
     private String courseDetails(@RequestParam("cid") Long cid, Model model){
@@ -595,4 +508,4 @@ public class CourseListController {
     }
 
 }
- */
+ 
