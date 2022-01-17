3b84f2c26f7ce4a4a1d3df35e8c4073fed960089
ZawMinbliss <96045323+ZawMinbliss@users.noreply.github.com>
2022-01-17 11:15:11 +0630

    add index course profile pictrure

--- src/main/java/com/blissstock/mappingSite/controller/IndexController.java
+++ src/main/java/com/blissstock/mappingSite/controller/IndexController.java
@@ -2,7 +2,12 @@ package com.blissstock.mappingSite.controller;
 
 import org.springframework.ui.Model;
 import java.util.*;
+
+import com.blissstock.mappingSite.dto.HomeCourseInfoDTO;
 import com.blissstock.mappingSite.entity.CourseInfo;
+import com.blissstock.mappingSite.entity.UserAccount;
+import com.blissstock.mappingSite.model.FileInfo;
+import java.nio.file.Path;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,14 +15,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.blissstock.mappingSite.repository.CourseRepository;
+import com.blissstock.mappingSite.repository.UserAccountRepository;
+import com.blissstock.mappingSite.service.StorageService;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
     private static final Logger logger = LoggerFactory.getLogger(CheckEmailController.class);
-
+    @Autowired
+    UserAccountRepository userAccRepo;
     @Autowired
     private CourseRepository courseRepo;
+    @Autowired
+    StorageService storageService;
 
     @GetMapping("/")
     private String getCourses(Model model) {
@@ -27,9 +38,53 @@ public class IndexController {
             List<CourseInfo> liveList = courseRepo.findByClassType("Live");
 
             List<CourseInfo> videoList = courseRepo.findByClassType("Video");
-            model.addAttribute("liveCourse", liveList);
 
-            model.addAttribute("recordedCourse", videoList);
+            List<HomeCourseInfoDTO> liveInfoDTOs = new ArrayList<HomeCourseInfoDTO>();
+            List<HomeCourseInfoDTO> videoInfoDTOs = new ArrayList<HomeCourseInfoDTO>();
+            ;
+
+            for (CourseInfo info : liveList) {
+                HomeCourseInfoDTO dto = new HomeCourseInfoDTO();
+                dto.setCourseID(info.getCourseId());
+                dto.setPrice(info.getFees());
+                dto.setTeacherName(info.getUserInfo().getUserName());
+                dto.setCourseName(info.getCourseName());
+                FileInfo fileInfo = loadProfile(info.getUserInfo().getUid());
+
+                // if profile is not found set as place holder
+                if (fileInfo == null) {
+                    dto.setProfilePic(new FileInfo("https://via.placeholder.com/150",
+                            "https://via.placeholder.com/150"));
+                } else {
+
+                    dto.setProfilePic(fileInfo);
+                }
+                liveInfoDTOs.add(dto);
+
+            }
+            for (CourseInfo info : videoList) {
+                HomeCourseInfoDTO dto = new HomeCourseInfoDTO();
+                dto.setCourseID(info.getCourseId());
+                dto.setPrice(info.getFees());
+                dto.setCourseName(info.getCourseName());
+                dto.setTeacherName(info.getUserInfo().getUserName());
+                dto.setProfilePic(loadProfile(info.getUserInfo().getUid()));
+                FileInfo fileInfo = loadProfile(info.getUserInfo().getUid());
+
+                // if profile is not found set as place holder
+                if (fileInfo == null) {
+                    dto.setProfilePic(new FileInfo("https://via.placeholder.com/150",
+                            "https://via.placeholder.com/150"));
+                } else {
+
+                    dto.setProfilePic(fileInfo);
+                }
+                videoInfoDTOs.add(dto);
+            }
+
+            model.addAttribute("liveCourse", liveInfoDTOs);
+            // todo change to video lsit
+            model.addAttribute("recordedCourse", liveInfoDTOs);
 
             // System.out.print(liveList.toString());
         } catch (Exception e) {
@@ -38,6 +93,27 @@ public class IndexController {
         }
 
         return "index.html";
+    }
+
+    // from Profile Controller;
+    // Get profile photo
+    private FileInfo loadProfile(long userId) {
+        try {
+            UserAccount userAcc = userAccRepo.findById(userId).orElse(null);
+            Path path = storageService.loadProfile(userAcc.getPhoto());
+            String name = path.getFileName().toString();
+            String url = MvcUriComponentsBuilder
+                    .fromMethodName(
+                            FileController.class,
+                            "getProfile",
+                            path.getFileName().toString())
+                    .build()
+                    .toString();
+
+            return new FileInfo(name, url);
+        } catch (Exception e) {
+            return null;
+        }
     }
 
 }
