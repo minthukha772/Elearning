447a6d32b57c458db9cf63a1443cd3efc3929c67
ZawMinbliss <96045323+ZawMinbliss@users.noreply.github.com>
2022-01-17 11:24:37 +0630

    typo fix

--- src/main/java/com/blissstock/mappingSite/controller/courseRegistrationController.java
+++ src/main/java/com/blissstock/mappingSite/controller/courseRegistrationController.java
@@ -37,44 +37,44 @@ public class CourseRegistrationController {
     @Autowired
     private UserInfoRepository userInfoRepository;
 
-    private List<CourseTime> ctList = new ArrayList<>(); 
-    
-    @RequestMapping(value={"/teacher/course-registration","/admin/course-registration"})
-    private String courseRegistration(Model model ){
+    private List<CourseTime> ctList = new ArrayList<>();
+
+    @RequestMapping(value = { "/teacher/course-registration", "/admin/course-registration" })
+    private String courseRegistration(Model model) {
         CourseInfo courseInfo = new CourseInfo();
-       
-        List<CourseTime> courseTimeList = new ArrayList<>(7);  
+
+        List<CourseTime> courseTimeList = new ArrayList<>(7);
         courseInfo.setCourseTime(courseTimeList);
-        
+
         model.addAttribute("course", courseInfo);
-  
+
         return "AT00001_CourseRegisteration";
     }
-    
+
     @PostMapping("/teacher/courseregister-confirm")
     private String courseRegistrationConfirm(@ModelAttribute("course") CourseInfo course,
-                                            @ModelAttribute("day0") String day0,
-                                            @ModelAttribute("startTime0") String startTime0,
-                                            @ModelAttribute("endTime0") String endTime0,
-                                            @ModelAttribute("day1") String day1,
-                                            @ModelAttribute("startTime1") String startTime1,
-                                            @ModelAttribute("endTime1") String endTime1,
-                                            @ModelAttribute("day2") String day2,
-                                            @ModelAttribute("startTime2") String startTime2,
-                                            @ModelAttribute("endTime2") String endTime2,
-                                            @ModelAttribute("day3") String day3,
-                                            @ModelAttribute("startTime3") String startTime3,
-                                            @ModelAttribute("endTime3") String endTime3,
-                                            @ModelAttribute("day4") String day4,
-                                            @ModelAttribute("startTime4") String startTime4,
-                                            @ModelAttribute("endTime4") String endTime4,
-                                            @ModelAttribute("day5") String day5,
-                                            @ModelAttribute("startTime5") String startTime5,
-                                            @ModelAttribute("endTime5") String endTime5,
-                                            @ModelAttribute("day6") String day6,
-                                            @ModelAttribute("startTime6") String startTime6,
-                                            @ModelAttribute("endTime6") String endTime6,
-                                            Model model){
+            @ModelAttribute("day0") String day0,
+            @ModelAttribute("startTime0") String startTime0,
+            @ModelAttribute("endTime0") String endTime0,
+            @ModelAttribute("day1") String day1,
+            @ModelAttribute("startTime1") String startTime1,
+            @ModelAttribute("endTime1") String endTime1,
+            @ModelAttribute("day2") String day2,
+            @ModelAttribute("startTime2") String startTime2,
+            @ModelAttribute("endTime2") String endTime2,
+            @ModelAttribute("day3") String day3,
+            @ModelAttribute("startTime3") String startTime3,
+            @ModelAttribute("endTime3") String endTime3,
+            @ModelAttribute("day4") String day4,
+            @ModelAttribute("startTime4") String startTime4,
+            @ModelAttribute("endTime4") String endTime4,
+            @ModelAttribute("day5") String day5,
+            @ModelAttribute("startTime5") String startTime5,
+            @ModelAttribute("endTime5") String endTime5,
+            @ModelAttribute("day6") String day6,
+            @ModelAttribute("startTime6") String startTime6,
+            @ModelAttribute("endTime6") String endTime6,
+            Model model) {
         logger.info("POST requested");
 
         List<CourseTime> courseTimeList = new ArrayList<>();
@@ -85,47 +85,47 @@ public class CourseRegistrationController {
         CourseTime courseTime4 = new CourseTime();
         CourseTime courseTime5 = new CourseTime();
         CourseTime courseTime6 = new CourseTime();
-        if(course.getClassType().equals("live")){
+        if (course.getClassType().equals("live")) {
 
             model.addAttribute("classActiveLive", true);
-            
-            if(!day0.equals("")){
+
+            if (!day0.equals("")) {
                 courseTime0.setCourseDays(day0);
                 courseTime0.setCourseStartTime(startTime0);
                 courseTime0.setCourseEndTime(endTime0);
                 courseTimeList.add(courseTime0);
             }
-            if(!day1.equals("")){
+            if (!day1.equals("")) {
                 courseTime1.setCourseDays(day1);
                 courseTime1.setCourseStartTime(startTime1);
                 courseTime1.setCourseEndTime(endTime1);
                 courseTimeList.add(courseTime1);
             }
-            if(!day2.equals("")){
+            if (!day2.equals("")) {
                 courseTime2.setCourseDays(day2);
                 courseTime2.setCourseStartTime(startTime2);
                 courseTime2.setCourseEndTime(endTime2);
                 courseTimeList.add(courseTime2);
             }
-            if(!day3.equals("")){
+            if (!day3.equals("")) {
                 courseTime3.setCourseDays(day3);
                 courseTime3.setCourseStartTime(startTime3);
                 courseTime3.setCourseEndTime(endTime3);
                 courseTimeList.add(courseTime3);
             }
-            if(!day4.equals("")){
+            if (!day4.equals("")) {
                 courseTime4.setCourseDays(day4);
                 courseTime4.setCourseStartTime(startTime4);
                 courseTime4.setCourseEndTime(endTime4);
                 courseTimeList.add(courseTime4);
             }
-            if(!day5.equals("")){
+            if (!day5.equals("")) {
                 courseTime5.setCourseDays(day5);
                 courseTime5.setCourseStartTime(startTime5);
                 courseTime5.setCourseEndTime(endTime5);
                 courseTimeList.add(courseTime5);
             }
-            if(!day6.equals("")){
+            if (!day6.equals("")) {
                 courseTime6.setCourseDays(day6);
                 courseTime6.setCourseStartTime(startTime6);
                 courseTime6.setCourseEndTime(endTime6);
@@ -135,34 +135,35 @@ public class CourseRegistrationController {
             model.addAttribute("courseTimeList", courseTimeList);
             ctList = courseTimeList;
             System.out.println("Heehee" + ctList);
-            
-        }else {
+
+        } else {
             model.addAttribute("classActiveVideo", true);
         }
-        
+
         model.addAttribute("course", course);
-        
-        // System.out.println("Heehee" + day0 + " " + startTime0 + " " + endTime0 + " " + day1 + " " + startTime1 + " " + endTime1);
-        // System.out.println("Haahaa " + courseTimeList.get(0).getCourseDays() + courseTimeList.size());
-        
+
+        // System.out.println("Heehee" + day0 + " " + startTime0 + " " + endTime0 + " "
+        // + day1 + " " + startTime1 + " " + endTime1);
+        // System.out.println("Haahaa " + courseTimeList.get(0).getCourseDays() +
+        // courseTimeList.size());
+
         return "AT00002_CourseRegisterationConfirm";
     }
 
     @PostMapping("/teacher/save-course-register")
-    private String saveCourseRegister(@ModelAttribute("course") CourseInfo course){
+    private String saveCourseRegister(@ModelAttribute("course") CourseInfo course) {
         course.setUserInfo(userInfoRepository.findById(userSessionService.getId()).get());
         logger.info("");
-        course.setIsCourseApproved(true); //was string "true"
+        course.setIsCourseApproved(true); // was string "true"
         courseRepo.save(course);
         System.out.println("HoeHoe" + ctList);
-        for(CourseTime courseTime : ctList){
+        for (CourseTime courseTime : ctList) {
             courseTime.setCourseInfo(course);
             courseTimeRepo.save(courseTime);
         }
-        
+
         return "CM0001_CompleteScreen";
-        //return "takealeave";
+        // return "takealeave";
     }
 
-    
 }
