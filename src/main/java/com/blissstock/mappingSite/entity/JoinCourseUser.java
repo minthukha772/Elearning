cc5a0488ba40e9c405e6ec412aabf77f824c7306
SaiHormKham-BlissStock <69182517+SaiHormKham-BlissStock@users.noreply.github.com>
2022-01-26 22:22:34 +0630

    debug: Change to IDENTITY

--- src/main/java/com/blissstock/mappingSite/entity/JoinCourseUser.java
+++ src/main/java/com/blissstock/mappingSite/entity/JoinCourseUser.java
@@ -33,7 +33,7 @@ public class JoinCourseUser {
 
   @Column(name = "join_id")
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long joinId;
 
   // mapping
