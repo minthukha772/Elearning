e1bc329a75f295397b427346fa7add00498c2f27
SaiHormKham-BlissStock <69182517+SaiHormKham-BlissStock@users.noreply.github.com>
2022-02-10 15:34:19 +0630

    debug: mail verification

--- src/main/java/com/blissstock/mappingSite/config/MyAuthenticationFailureHandler.java
+++ src/main/java/com/blissstock/mappingSite/config/MyAuthenticationFailureHandler.java
@@ -9,13 +9,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.blissstock.mappingSite.exceptions.NonMailVerifiedUserException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
 public class MyAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
@@ -34,13 +34,13 @@ public class MyAuthenticationFailureHandler extends SimpleUrlAuthenticationFailu
         String redirectURL = "/login?error";
         logger.info("before redirectURL {}",redirectURL);
 
-       if(exception.getMessage().toLowerCase().contains("suspend")) {
+        if(exception.getMessage().toLowerCase().contains("suspend")){
             redirectURL+="=suspended";
+        }else if(exception.getMessage().toLowerCase().contains("not verified")){
+            redirectURL+="=non-email";
         }
-
+       
         logger.info("redirectURL {}",redirectURL);
-
-
 
         super.setDefaultFailureUrl(redirectURL);
         super.onAuthenticationFailure(request, response, exception);
