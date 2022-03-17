c9fbdbebd611c48015788581123df6d5a19c13a5
Zaw Min Thein <zawmin.thein.u20@bliss-stock.com>
2022-03-17 22:22:04 +0630

    fix course edit not showing course types

--- src/main/java/com/blissstock/mappingSite/controller/CourseEditController.java
+++ src/main/java/com/blissstock/mappingSite/controller/CourseEditController.java
@@ -63,7 +63,7 @@ public class CourseEditController {
         if (course != null) {
             // List<CourseTime> courseTimeList = new ArrayList<>();
 
-            if (course.getClassType().equals("LIVE")) {
+            if (course.getClassType().toUpperCase().equals("LIVE")) {
 
                 model.addAttribute("classActiveLive", true);
 
@@ -195,7 +195,7 @@ public class CourseEditController {
         CourseTime courseTime4 = new CourseTime();
         CourseTime courseTime5 = new CourseTime();
         CourseTime courseTime6 = new CourseTime();
-        if (course.getClassType().equals("LIVE")) {
+        if (course.getClassType().toUpperCase().equals("LIVE")) {
 
             model.addAttribute("classActiveLive", true);
 
@@ -298,7 +298,7 @@ public class CourseEditController {
         CourseInfo updateCourse = courseInfoRepo.findById(course.getCourseId()).get();
         logger.info("Get Requested {}", updateCourse);
         System.out.print("Update Info: " + updateCourse);
-        if (classType.equals("LIVE")) {
+        if (classType.toUpperCase().equals("LIVE")) {
             System.out.print("It works!");
             updateCourse.setMaxStu(course.getMaxStu());
             updateCourse.setStartDate(course.getStartDate());
