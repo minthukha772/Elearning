c767eed81698c97bb7bf576b8cc67b7c16773fbf
ZawMinbliss <zawmin.thein.u20@bliss-stock.com>
2022-06-03 22:39:29 +0630

    redirect port

--- src/main/java/com/blissstock/mappingSite/config/TomcatSever.java
+++ src/main/java/com/blissstock/mappingSite/config/TomcatSever.java
@@ -14,32 +14,31 @@ import org.slf4j.LoggerFactory;
 @Configuration
 public class TomcatSever {
 
-    // @Bean
-    // public ServletWebServerFactory servletContainer() {
+    @Bean
+    public ServletWebServerFactory servletContainer() {
 
-    // TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
 
-    // @Override
-    // protected void postProcessContext(Context context) {
-    // SecurityConstraint securityConstraint = new SecurityConstraint();
-    // securityConstraint.setUserConstraint("CONFIDENTIAL");
-    // SecurityCollection collection = new SecurityCollection();
-    // collection.addPattern("/*");
-    // securityConstraint.addCollection(collection);
-    // context.addConstraint(securityConstraint);
-    // }
-    // };
-    // tomcat.addAdditionalTomcatConnectors(redirectConnector());
-    // return tomcat;
-    // }
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
 
-    // private Connector redirectConnector() {
-    // Connector connector = new
-    // Connector("org.apache.coyote.http11.Http11NioProtocol");
-    // connector.setScheme("http");
-    // connector.setPort(8080);
-    // connector.setSecure(false);
-    // connector.setRedirectPort(8443);
-    // return connector;
-    // }
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
 }
