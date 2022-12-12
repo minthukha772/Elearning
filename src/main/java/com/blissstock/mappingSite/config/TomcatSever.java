1db6d161114a388ec78ad41036cd46a0f78eb8c7
zinlinhtet-bs <zin.linhtet.b27@bliss-stock.com>
2022-12-12 11:19:18 +0630

    fixing tomcat

--- src/main/java/com/blissstock/mappingSite/config/TomcatSever.java
+++ src/main/java/com/blissstock/mappingSite/config/TomcatSever.java
@@ -4,8 +4,7 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.*;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import
-org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
