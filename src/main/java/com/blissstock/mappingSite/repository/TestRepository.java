74582114ec82de142a9654f1e7d62530d5af5d41
myatkokog32 <114830617+myatkokog32@users.noreply.github.com>
2023-03-23 18:41:38 +0900

    update UI

--- src/main/java/com/blissstock/mappingSite/repository/TestRepository.java
+++ src/main/java/com/blissstock/mappingSite/repository/TestRepository.java
@@ -28,16 +28,4 @@ public interface TestRepository extends JpaRepository<Test, Long> {
 
         @Query(value = "Select * from test where user_info_account_id = :user_id order by test_id desc", nativeQuery = true)
         public List<Test> getListByUser(@Param("user_id") Long user_id);
-
-        @Query(value = "insert into test(date, description, end_time, exam_status, minutes_allowed, passing_score_percent,"
-                        +
-                        "section_name, start_time, course_info_course_id, user_info_account_id) values(:date, :description, :end_time,"
-                        +
-                        " :exam_status, :minutes_allowed, :passing_score_percent, :section_name, :start_time, :course_id, :user_id)", nativeQuery = true)
-        public Void insertTest(@Param("date") String date, @Param("description") String description,
-                        @Param("end_time") String end_time,
-                        @Param("exam_status") String exam_status, @Param("minutes_allowed") Integer minutes_allowed,
-                        @Param("passing_score_percent") Integer passing_score_percent,
-                        @Param("section_name") String section_name, @Param("start_time") String start_time,
-                        @Param("course_id") Integer course_id, @Param("user_id") Long user_id);
 }
---
+++ src/main/resources/static/js/AT0004_ExamManagement.js
@@ -0,0 +1,6 @@
+let createExamContainer = document.querySelector('.create-exam-container');
+let cancelBtn = document.querySelector('#cancel_btn');
+let createExam = document.querySelector('#create_exam');
+
+cancelBtn.addEventListener('click',() => createExamContainer.classList.add('d-none') );
+createExam.addEventListener('click',() => createExamContainer.classList.remove('d-none') );
