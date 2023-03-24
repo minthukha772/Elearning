5f995df3c9fbbaf5998c0b93e56b10b3373dc017
myatkokog32 <114830617+myatkokog32@users.noreply.github.com>
2023-03-24 20:26:33 +0900

    update

--- src/main/resources/static/js/AT0004_ExamManagement.js
+++ src/main/resources/static/js/AT0004_ExamManagement.js
@@ -2,5 +2,8 @@ let createExamContainer = document.querySelector('.create-exam-container');
 let cancelBtn = document.querySelector('#cancel_btn');
 let createExam = document.querySelector('#create_exam');
 
-cancelBtn.addEventListener('click',() => createExamContainer.classList.add('d-none') );
-createExam.addEventListener('click',() => createExamContainer.classList.remove('d-none') );
+cancelBtn.addEventListener('click', () => createExamContainer.classList.add('d-none'));
+createExam.addEventListener('click', () => {
+    createExamContainer.classList.remove('d-none')
+    $("#exam_status_dropdown").css("display", "none");
+});
