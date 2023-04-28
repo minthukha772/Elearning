4125870c8a8c722a94e386fc058783a3bb417fe3
myatkokog32 <114830617+myatkokog32@users.noreply.github.com>
2023-04-28 10:45:09 +0900

    fix merge

--- src/main/java/com/blissstock/mappingSite/entity/TestStudent.java
+++ src/main/java/com/blissstock/mappingSite/entity/TestStudent.java
@@ -1,5 +1,10 @@
 package com.blissstock.mappingSite.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -7,49 +12,66 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
-@Table(name="test_student")
+@Table(name = "test_student")
 public class TestStudent {
-   
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @Column(name = "test_student_id")
+    private Long testStudentId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "exam_id")
-    private Exam exam;
+    @JoinColumn(name = "test_id")
+    private Test test;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private UserAccount userAccount;
+    private UserAccount user;
 
-    public long getId() {
-        return id;
+    @OneToMany(
+    fetch = FetchType.LAZY,
+    cascade = CascadeType.ALL,
+    mappedBy = "testStudent"
+    )
+    @JsonIgnore
+    private List<MarkedAnswer> markedAnswer = new ArrayList<>();
+
+    // Constructor, getters, and setters
+
+    public TestStudent() {}
+
+    public TestStudent(Test test, UserAccount user) {
+        this.test = test;
+        this.user = user;
     }
 
-    public void setId(long id) {
-        this.id = id;
+    public Long getTestStudentId() {
+        return testStudentId;
     }
 
-    public Exam getExam() {
-        return exam;
+    public void setTestStudentId(Long testStudentId) {
+        this.testStudentId = testStudentId;
     }
 
-    public void setExam(Exam exam) {
-        this.exam = exam;
+    public Test getTest() {
+        return test;
     }
 
-    public UserAccount getUserAccount() {
-        return userAccount;
+    public void setTest(Test test) {
+        this.test = test;
     }
 
-    public void setUserAccount(UserAccount userAccount) {
-        this.userAccount = userAccount;
+    public UserAccount getUser() {
+        return user;
     }
-    
 
-
-    
+    public void setUser(UserAccount user) {
+        this.user = user;
+    }
 }
