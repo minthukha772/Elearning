41480e9a651eb34f1df9c015070f0c016b562659
minthukha <minthukha312@gmail.com>
2023-08-16 01:04:51 +0630

    update testexamineerepository

--- src/main/java/com/blissstock/mappingSite/repository/TestExamineeRepository.java
+++ src/main/java/com/blissstock/mappingSite/repository/TestExamineeRepository.java
@@ -20,4 +20,7 @@ public interface TestExamineeRepository extends JpaRepository<TestExaminee, Long
 
     @Query(value = "Select * from test_examinee where test_id = :test_id and examinee_student_id = :uid limit 1", nativeQuery = true)
     public TestExaminee findByTestIdAndUid(@Param("test_id") Long test_id, @Param("uid") Long uid);
+
+    @Query(value = "Select * from test_examinee where test_id = :test_id and examinee_guest_id = :uid limit 1", nativeQuery = true)
+    public TestExaminee findByTestIdAndUidGuest(@Param("test_id") Long test_id, @Param("uid") Long uid);
 }
