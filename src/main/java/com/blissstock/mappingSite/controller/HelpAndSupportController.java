f31f36c261ef8fd70fb52b8812190b7a545bb9e6
minthukha <minthukha312@gmail.com>
2023-11-14 12:33:07 +0630

    code close

--- src/main/java/com/blissstock/mappingSite/controller/HelpAndSupportController.java
+++ src/main/java/com/blissstock/mappingSite/controller/HelpAndSupportController.java
@@ -21,6 +21,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.blissstock.mappingSite.entity.Contact;
 import com.blissstock.mappingSite.entity.CourseInfo;
@@ -66,47 +68,56 @@ public class HelpAndSupportController {
         return "contactAdmin.html";
     }
 
-    @PostMapping(value = { "/saveInquiry", "/guest/saveInquiry" })
-    private String saveInquiry(Model model, @RequestBody String payload) {
-        try {
-            JSONObject jsonObject = new JSONObject(payload);
-            Contact contact = new Contact();
-            String name = jsonObject.getString("name");
-            String email = jsonObject.getString("email");
-            String number = jsonObject.getString("number");
-            String details = jsonObject.getString("details");
-            String description = jsonObject.getString("description");
-            // Contact contact = new
-            // Contact(null,name,email,number,details,description,null);
-            contact.setName(name);
-            contact.setEmail(email);
-            contact.setPhoneNumber(number);
-            contact.setDetails(details);
-            contact.setDescription(description);
-            contact.setInquiryDate(new Date());
-            // Contact contact = new Contact(null,name,email,number,details,description);
-            contactRepository.save(contact);
-
-            return "redirect:/email-confirmscreen";
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    @GetMapping("/email-confirmscreen")
-    public String confirmScreen(Model model) {
-        Long userId = 52L;
-
-        Contact contact = contactRepository.findById(userId).orElse(null);
-        String name = contact.getName();
-
-        model.addAttribute("name", name);
-
-        return "CM0011_EmailConfirm.html";
-
-    }
+    /*
+     * @PostMapping(value = { "/saveInquiry", "/guest/saveInquiry" })
+     * private String saveInquiry(Model model, @RequestBody String payload) {
+     * try {
+     * JSONObject jsonObject = new JSONObject(payload);
+     * Contact contact = new Contact();
+     * String name = jsonObject.getString("name");
+     * String email = jsonObject.getString("email");
+     * String number = jsonObject.getString("number");
+     * String details = jsonObject.getString("details");
+     * String description = jsonObject.getString("description");
+     * // Contact contact = new
+     * // Contact(null,name,email,number,details,description,null);
+     * contact.setName(name);
+     * contact.setEmail(email);
+     * contact.setPhoneNumber(number);
+     * contact.setDetails(details);
+     * contact.setDescription(description);
+     * contact.setInquiryDate(new Date());
+     * // Contact contact = new Contact(null,name,email,number,details,description);
+     * contactRepository.save(contact);
+     * 
+     * // return "redirect:/CM0011_EmailConfirm.html";
+     * // return new RedirectView("/CM0011_EmailConfirm");
+     * // return "CM0011_EmailConfirm.html";
+     * 
+     * // return "redirect:/email-confirmscreen";
+     * return "redirect:/guest/saveInquiry/confirm";
+     * 
+     * } catch (Exception e) {
+     * e.printStackTrace();
+     * }
+     * return null;
+     * }
+     * 
+     * @GetMapping("/guest/saveInquiry/confirm")
+     * public String confirmScreen(Model model) {
+     * /*
+     * Long userId = 52L;
+     * 
+     * Contact contact = contactRepository.findById(userId).orElse(null);
+     * String name = contact.getName();
+     * 
+     * model.addAttribute("name", name);
+     * 
+     * // return "CM0011_EmailConfirm.html";
+     * 
+     * return "CM0011_EmailConfirm.html";
+     * }
+     */
 
     private Long getUid() {
         Long uid = userSessionService.getUserAccount().getAccountId();
