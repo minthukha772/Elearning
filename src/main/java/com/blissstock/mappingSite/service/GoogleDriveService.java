79c4bcf497cc2fb50e554902a1e245f95b107dab
myatkokog32 <114830617+myatkokog32@users.noreply.github.com>
2023-11-20 17:15:09 +0900

    fix google drive service

--- src/main/java/com/blissstock/mappingSite/service/GoogleDriveService.java
+++ src/main/java/com/blissstock/mappingSite/service/GoogleDriveService.java
@@ -59,7 +59,7 @@ public class GoogleDriveService {
             "    \"token_uri\": \"https://oauth2.googleapis.com/token\",\n" +
             "    \"auth_provider_x509_cert_url\": \"https://www.googleapis.com/oauth2/v1/certs\",\n" +
             "    \"client_secret\": \"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\",\n" +
-            "    \"redirect_uris\": [\"https://pyinnyarsubuu.com/Callback\"]\n" +
+            "    \"redirect_uris\": [\"https://34.146.220.198:8888/Callback\"]\n" +
             "  }\n" +
             "}";
 
@@ -82,7 +82,7 @@ public class GoogleDriveService {
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8080).build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
         // returns an authorized Credential object.
         return credential;
